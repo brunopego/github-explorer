65a52c2172b1c3d3b0b110e0abac7251adf08d85
Bruno Lacerda <brunolcpg@gmail.com>
2020-04-20 18:04:59 -0300

    Add Dashboard page and its styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/12010387?s=460&u=5e930b2adf0ad801fda2fb70d2a684a7e367b7d3&v=4"
+            alt="Bruno Lacerda"
+          />
+          <div>
+            <strong>brunopego/psumaa</strong>
+            <p>Sequenciamento em uma Máquina</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/12010387?s=460&u=5e930b2adf0ad801fda2fb70d2a684a7e367b7d3&v=4"
+            alt="Bruno Lacerda"
+          />
+          <div>
+            <strong>brunopego/psumaa</strong>
+            <p>Sequenciamento em uma Máquina</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/12010387?s=460&u=5e930b2adf0ad801fda2fb70d2a684a7e367b7d3&v=4"
+            alt="Bruno Lacerda"
+          />
+          <div>
+            <strong>brunopego/psumaa</strong>
+            <p>Sequenciamento em uma Máquina</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
