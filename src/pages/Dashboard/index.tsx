7abbd29302a340e1fd7c2a206cb571f592e0fda7
Bruno Lacerda <brunolcpg@gmail.com>
2020-04-20 18:16:43 -0300

    Removes unnecessary anchors

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -28,30 +28,6 @@ const Dashboard: React.FC = () => {
 
           <FiChevronRight size={20} />
         </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/12010387?s=460&u=5e930b2adf0ad801fda2fb70d2a684a7e367b7d3&v=4"
-            alt="Bruno Lacerda"
-          />
-          <div>
-            <strong>brunopego/psumaa</strong>
-            <p>Sequenciamento em uma Máquina</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/12010387?s=460&u=5e930b2adf0ad801fda2fb70d2a684a7e367b7d3&v=4"
-            alt="Bruno Lacerda"
-          />
-          <div>
-            <strong>brunopego/psumaa</strong>
-            <p>Sequenciamento em uma Máquina</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
       </Repositories>
     </>
   );
